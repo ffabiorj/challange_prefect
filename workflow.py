58126de57ae31bf00fba83b9acc637b1b7e1bfc8
Fábio Oliveira <fabio20rj@gmail.com>
2024-06-09 21:29:16 +0200

    update: change random method to choice

--- workflow.py
+++ workflow.py
@@ -3,13 +3,14 @@ from prefect import flow, task
 import random
 import time
 
+URL = "https://rickandmortyapi.com/api/"
+
 @task(retries=2, retry_delay_seconds=1)
 def get_character(name: str) -> dict:
     """Get info about a character"""
-    url = f"https://rickandmortyapi.com/api/character/?name={name}"
+    url = f"{URL}character/?name={name}"
     # simulate a task that rondomly fails due to a timeout
-    time_sleep = random.randint(1, 3)
-    time.sleep(time_sleep)
+    time_sleep = random.choice([1,3])
     if time_sleep > 2:
         raise Warning("Simulate task failure due to timeout")
     response = httpx.get(url)
@@ -17,24 +18,27 @@ def get_character(name: str) -> dict:
     character_info = response.json()
     return character_info
 
+
 @task
 def get_episodes(name: str) -> dict:
     """Get all episodies by name"""
-    url = f"https://rickandmortyapi.com/api/episode/?name={name}"
+    url = f"{URL}episode/?name={name}"
     response = httpx.get(url)
     response.raise_for_status()
     episodes_info = response.json()
     return episodes_info
-    
+
+
 @flow
 def show_info(name: str) -> str:
     """show info about a character"""
-    get_character_name = get_character(name)
-    character_name = get_character_name["results"][0]["name"].split(" ")[0]
-    episodes = get_episodes(character_name)
+    character = get_character(name)
+    get_character_name = character["results"][0]["name"]
+    get_first_name = get_character_name.split(" ")[0]
+    episodes = get_episodes(get_first_name)
     episodes = episodes["info"]["count"]
-    print(f"This character {character_name} has {episodes} episodes")
+    print(f"This character {get_character_name} has {episodes} episodes")
+
 
 if __name__ == "__main__":
-    show_info('rick')
-    
+    show_info("rick")
